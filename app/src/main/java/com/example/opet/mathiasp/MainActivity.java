7f37c4bd531413f24ec1b14d622aeca042a0b90e
Mathias <gmathias1717@gmail.com>
2018-02-28 11:22:13 -0300

    Comitadasso

--- app/src/main/java/com/example/opet/mathiasp/MainActivity.java
+++ app/src/main/java/com/example/opet/mathiasp/MainActivity.java
@@ -3,15 +3,20 @@ package com.example.opet.mathiasp;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-    private TextView textResultado;
     private EditText editNome;
-
+    private CheckBox checkNews;
+    private Spinner spinnerFrutas;
+    private RadioGroup radioSelecao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +24,28 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         editNome = findViewById(R.id.editNome);
+        checkNews = findViewById(R.id.checkNews);
+        spinnerFrutas = findViewById(R.id.spinnerFrutas);
+        radioSelecao = findViewById(R.id.grupoSelecao);
     }
 
     public void click(View v){
         String data = editNome.getText().toString();
+        if(checkNews.isChecked()){
+            data += "\nAssinado!";
+        }
+        else
+        {
+            data += "\nNão Assinado!";
+        }
+
+        int selecionado = radioSelecao.getCheckedRadioButtonId();
+        RadioButton radioButton = findViewById(selecionado);
+
+        data += "\n"+spinnerFrutas.getSelectedItem().toString();
+
+        data+= "\n"+ radioButton.getText().toString();
+
         Toast.makeText(this, data, Toast.LENGTH_LONG).show();
     }
 }
